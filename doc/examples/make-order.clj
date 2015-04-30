9c3f6449a82b0f1d95b9c1a4d48d6585ceedf65e
Foldi Tamas <tfoldi@starschema.net>
2015-04-30 07:45:35 +0200

    Complete implementing table function. Parse-table-definition is still missing.

--- doc/examples/make-order.clj
+++ doc/examples/make-order.clj
@@ -1,8 +1,23 @@
+(require [clj-tableau/dataextract])
+
 (def test-data
   []
   )
 
+(def table-definition
+  {
+   :collation Collation/EN_GB
+   :columns   [
+               "Purchased"        Type/DATETIME
+               "Product"          Type/CHAR_STRING
+               "uProduct"         Type/UNICODE_STRING
+               "Price"            Type/DOUBLE
+               "Quantity"         Type/INTEGER
+               "Taxed"            Type/BOOLEAN
+               "Expiration Date"  Type/DATE
+               "Produkt"         [Type/CHAR_STRING Collation/DE]
+               ]})
 
 (with-extract [foo "foobar.tde"]
-              (println "a")
+              (table foo "Extract" table-definition)
               )
