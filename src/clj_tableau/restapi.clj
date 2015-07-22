a114d47a4f2a31e383fd875f523a302756a7acd7
buub <roland.vr87@gmail.com>
2015-07-22 17:53:39 +0200

    Pagination simplified. Added ability to query user data.

--- src/clj_tableau/restapi.clj
+++ src/clj_tableau/restapi.clj
@@ -1,5 +1,5 @@
 (ns clj-tableau.restapi
-  (:require [clj-http.client :as client]
+  (:require [clj-http.client]
             [clojure.zip :refer [xml-zip]]
             [clojure.data.zip.xml :refer [xml-> xml1-> attr text]]
             [clojure.data.xml :as xml]
@@ -28,13 +28,12 @@
         xml/parse-str
         xml-zip)))
 
-
 (defn- get-status-from-http-exception
   "Get HTTP exception code from a clojure exception info throwed by clj-http"
   [e]
   (get (ex-data e) :status))
 
-(defn- http
+(defn http
   "Perform a http call to tableau server. Host can be hostname or session"
   [method host api-path http-params]
   (get-zip
@@ -50,10 +49,6 @@
                  (xml/element :credentials {:name     name
                                             :password password}
                               (xml/element :site {:contentUrl site})))))
-
-(defn- get-users-from-ts-response
-  [ts-response]
-  (xml-> ts-response :users :user (juxt (attr :name) (attr :id))))
 
 (defn- get-users-from-tableau-response
   [ts-response]
@@ -117,7 +112,6 @@
         (log/debug "User " name " already added to this site, ignoring request.")
         (throw e)))))
 
-
 (defn logon-to-server
   "Logon to tableau server by invoking /auth/signin, returns map with token,
   site id and hostname"
@@ -138,76 +132,80 @@
                           :site (attr :id))
      :host        host}))
 
-
-(defn get-users-on-site
-  "Iterates on site users defined by session. Page size is 100"
-  [session]
-  (loop [page-number 1 allusers '()]
-    (let [ts-response (http "get" session "/users/"
-                            {:query-params {:pageSize   page-size
-                                            :pageNumber page-number}})
-          users (get-users-from-ts-response ts-response)]
-
-      ;      (log/debug (str "On page " page-number " -> " users))
-      (if (xml1-> ts-response
-                  :pagination)
-        (if (> (* page-size page-number) (read-string (xml1-> ts-response
-                                                              :pagination
-                                                              (attr :totalAvailable))))
-          (do
-            (log/debug "All users downloaded")
-            (->>
-              (conj allusers users)
-              (flatten)
-              (apply hash-map)))
-          (recur (inc page-number) (conj allusers users)))))))
-
 (defn signout
   "Logoff from server"
   [session]
   (http "post" session "/auth/signout" {}))
 
+(defmacro with-tableau-rest-api
+  "Initialized a new tableau session and ensures that session will be terminated when
+   the processing goes out of scope. Usage:
+   (with-tableau-rest-api [conn '(host site name password)] ( rest-call(conn p1 p2 )) "
+  [bindings & body]
+  (let [form (bindings 0) params (bindings 1)]
+    `(let [~form (apply logon-to-server ~params)]
+       (try
+         ~@body
+         (finally
+           (signout ~form))))))
+
+(defn- get-paginated-resource
+  "Get all element from a paginated resource using page-size"
+  [session resource collector reducer]
+  (loop [page-number 1 all-elements '()]
+    (let [ts-response (http "get" session (str "/" resource "/")
+                            {:query-params {:pageSize   page-size
+                                            :pageNumber page-number}})
+          collection (conj all-elements (collector ts-response))]
+
+      (if (xml1-> ts-response
+                  :pagination)
+        (if (> (* page-size page-number) (read-string (xml1-> ts-response
+                                                              :pagination
+                                                              (attr :totalAvailable))))
+          (reducer collection)
+          (recur (inc page-number) collection))))))
+
+
+(defn get-users-on-site
+  "Iterates on site users defined by session."
+  [session]
+  (get-paginated-resource session "users" get-users-from-tableau-response
+                          (fn [userid-name-pairs]
+                            (->> userid-name-pairs
+                                 (flatten)
+                                 (apply hash-map)))))
+
+
+(defn get-users-from-group
+  "Iterates on site users defined by session."
+  [session group-id]
+  (get-paginated-resource session (str "groups/" group-id "/users") get-users-from-tableau-response
+                          (fn [userid-name-pairs]
+                            (->> userid-name-pairs
+                                 (flatten)
+                                 (apply hash-map)))))
+
+(defn get-groups-on-site
+  "Get all group ids and names from the site"
+  [session]
+  (get-paginated-resource session "groups"
+                          (fn [response]
+                            (xml-> response
+                                   :groups
+                                   :group
+                                   (juxt (attr :id) (attr :name))))
+                          (fn [groupid-name-pairs]
+                            (apply hash-map (flatten groupid-name-pairs)))))
+
 (defn get-group-id
   "Return the group-id of a specific group
   GET /api/api-version/sites/site-id/groups"
   [session group]
-  (let [response
-        (http "get" session "groups" {:query-params {:pageSize 1000}})]
-    (->>
-      (xml-> response
-             :groups
-             :group
-             (juxt (attr :id) (attr :name)))
-      (vec)
-      (partition 2)
-      (filter #(= group (second %)))
-      (first)
-      (first))))
-
-(defn get-users-from-group
-  "Gets users from Tableau group using the Tableau 9 REST API
-  GET /api/api-version/sites/site-id/groups/group-id/users"
-  [session group-id]
-  (loop [page-number 1 group-members '()]
-    (let [response
-          (http "get" session (str "groups/" group-id "/users")
-                {:query-params {:pageSize   page-size
-                                :pageNumber page-number}})
-          users (xml-> response
-                       :users
-                       :user
-                       (juxt (attr :id) (attr :name)))]
-
-      (if (> (* page-size page-number) (read-string (xml1-> response
-                                                            :pagination
-                                                            (attr :totalAvailable))))
-        (do
-          (log/debug "All users downloaded from tableau group " group-id)
-          (->>
-            (conj group-members users)
-            (flatten)
-            (apply hash-map)))
-        (recur (inc page-number) (conj group-members users))))))
+  (->> (get-groups-on-site session)
+       (filter #(= group (second %)))
+       (first)
+       (first)))
 
 (defn add-user-to-tableau-group
   "Add users to Tableau group
@@ -235,3 +233,14 @@
       (log/error (type e) ": Error when removing user " user-id " " (ex-data e))
       (throw e))))
 
+(defn query-user-on-site
+  "Get name, role, email, full name from tableau user
+  GET /api/api-version/sites/site-id/users/user-id"
+  [session user-id]
+  (let [ts-response (http "get" session (str "users/" user-id) {})]
+    (zipmap
+      ["id" "name" "siteRole" "lastLogin" "fullName"]
+      (xml->
+        ts-response
+        :user
+        (juxt (attr :id) (attr :name) (attr :siteRole) (attr :lastLogin) (attr :fullName))))))
