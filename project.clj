d2b32bb3c0a004e610182a15ea762582f20ecbed
Foldi Tamas <tfoldi@starschema.net>
2015-08-01 12:02:13 +0200

    Change version to 1.0.3 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-tableau "1.0.3-SNAPSHOT"
+(defproject clj-tableau "1.0.3"
             :description "Clojure bindings for Tableau APIs (Extract & Rest)"
             :url "http://github.com/starschema/clj-tableau"
             :license {:name "Eclipse Public License"
